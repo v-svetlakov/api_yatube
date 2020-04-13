6352c8266bdb288942eef818b0a08ac838d31b58
Владимир <svetlakov454@gmail.com>
2020-04-13 09:35:29 +0300

    2

--- api/permissions.py
+++ api/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsOwnerOrReadOnly(permissions.IsAuthenticated):
 
 
     def has_object_permission(self, request, view, obj):
--- api/views.py
+++ api/views.py
@@ -1,4 +1,3 @@
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .permissions import IsOwnerOrReadOnly
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
@@ -9,18 +8,18 @@ from .serializers import PostSerializer, CommentSerializer
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthenticated,
-                          IsOwnerOrReadOnly]
+    permission_classes = [
+        IsOwnerOrReadOnly
+    ]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly,
-                          IsAuthenticated,
-                          IsOwnerOrReadOnly
-                          ]
+    permission_classes = [
+        IsOwnerOrReadOnly
+    ]
     def perform_create(self, serializer):
         post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
         serializer.save(author=self.request.user, post=post)
